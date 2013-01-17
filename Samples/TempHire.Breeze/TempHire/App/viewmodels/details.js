e87dd933ffec4f536a6dfbd1ae2555374a3058fb
Marcel Good <marcelg@ideablade.com>
2013-01-17 14:03:12 -0800

    Add TempHire.Breeze sample. An initial migration of TempHire Cocktail to Breeze

---
+++ Samples/TempHire.Breeze/TempHire/App/viewmodels/details.js
@@ -0,0 +1,77 @@
+﻿define(['services/unitofwork', 'logger', 'durandal/system', 'durandal/viewModel', 'viewmodels/contacts', 'durandal/app'],
+    function(unitofwork, logger, system, viewModel, contacts, app) {
+
+        return (function() {
+
+            var details = function(id) {
+                this.title = 'Details View';
+                this.staffingResourceId = id;
+                this.staffingResource = ko.observable();
+                this.initialized = ko.observable();
+                this.hasNothingToSave = ko.observable(true);
+                
+                var ref = unitofwork.get(id);
+                this.unitOfWork = ref.value();
+
+                this.contacts = viewModel.activator();
+            };
+
+            details.prototype.activate = function () {
+                this.contacts.activate();
+
+                if (this.initialized())
+                    return true;
+                
+                // Subscribe to events
+                app.on('hasChanges', function() {
+                    this.hasNothingToSave(!this.unitOfWork.hasChanges());
+                }, this);
+
+                var vm = this;
+                return this.unitOfWork.staffingResources.withId(this.staffingResourceId)
+                    .then(function(data) {
+                        vm.staffingResource(data);
+                        vm.log("StaffingResource loaded", true);
+                    })
+                    .then(function() {
+                        vm.contacts.activateItem(new contacts(vm.staffingResourceId));
+                    })
+                    .then(function () {
+                        vm.initialized(true);
+                        return true;
+                    })
+                    .fail(this.handleError);
+            };
+
+            details.prototype.deactivate = function(close) {
+                if (close) {
+                    unitofwork.get(this.staffingResourceId).release();
+                    app.off(null, null, this);
+                    this.initialized(false);
+                }
+
+                this.contacts.deactivate(close);
+
+                return true;
+            };
+
+            details.prototype.save = function() {
+                this.unitOfWork.commit().fail(this.handleError);
+            };
+
+            details.prototype.cancel = function() {
+                this.unitOfWork.rollback();
+            };
+
+            details.prototype.handleError = function(error) {
+                logger.log(error.message, null, system.getModuleId(this), true);
+                throw error;
+            };
+
+            details.prototype.log = function(message, showToast) {
+                logger.log(message, null, system.getModuleId(this), showToast);
+            };
+
+            return details;
+        })();
+    });
---
+++ Samples/TempHire.Breeze/TempHire/App/viewmodels/resourcemgt.js
@@ -0,0 +1,55 @@
+﻿define(['services/unitofwork', 'logger', 'durandal/system', 'durandal/viewModel', 'viewmodels/details'],
+    function (unitofwork, logger, system, viewModel, details) {
+
+        var staffingResources = ko.observableArray();
+        var uow = unitofwork.create();
+        var activeDetail = viewModel.activator();
+
+        var vm = {
+            title: "Resource Management",
+            staffingResources : staffingResources,
+            activeDetail : activeDetail,
+            activate: activate,
+            deactivate : deactivate,
+            viewAttached : viewAttached
+        };
+
+        return vm;
+        
+        function activate() {
+            activeDetail.activate();
+            
+            return uow.staffingResourceListItems.all()
+                .then(querySucceeded)
+                .fail(handleError);
+            
+            function querySucceeded(data) {
+                staffingResources(data);
+                log("StaffingResourceListItems loaded", true);
+            }
+        }
+        
+        function deactivate(close) {
+            return activeDetail.deactivate(close);
+        }
+        
+        function viewAttached(view) {
+            $(view).on('click', '.selectable-row', function() {
+                var staffingResource = ko.dataFor(this);
+
+                var detail = new details(staffingResource.id);
+                activeDetail.activateItem(detail);
+
+                return false;
+            });
+        }
+        
+        function handleError(error) {
+            logger.log(error.message, null, system.getModuleId(vm), true);
+            throw error;
+        }
+        
+        function log(message, showToast) {
+            logger.log(message, null, system.getModuleId(vm), showToast);
+        }
+    });
